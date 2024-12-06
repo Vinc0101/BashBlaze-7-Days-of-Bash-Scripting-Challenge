f881840e7a218bd1f1e3e51fcfb92cf1b61e7b6e
VincentVescoli <125880026+VincentVescoli@users.noreply.github.com>
2024-12-06 23:42:56 +0100

    day3

--- Challenges/Day_3/day_3.sh
+++ Challenges/Day_3/day_3.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "hello day 3"
+
+read -p "enter username:  " username
+
+if id "$username" &>/dev/null; then
+    echo "User already exists"
+else
+    read -p "enter password:  " password
+    sudo useradd -s /bin/bash "$username"
+    echo "$username:$password" | sudo chpasswd #chpasswd tkaes username and pass
+
+
+fi
+
+function create_user {
+
+    
+}
