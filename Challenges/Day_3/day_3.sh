7e3c79c8e84842d11f03942386e5e1fe994ba11a
VincentVescoli <125880026+VincentVescoli@users.noreply.github.com>
2024-12-07 22:38:44 +0100

    day 3 complete

--- Challenges/Day_3/day_3.sh
+++ Challenges/Day_3/day_3.sh
@@ -19,9 +19,31 @@ function create_user {
 }
 
 
-function delete_user {
+function reset_password {
 
     read -p "enter username:  " username
+
+    if ! id "$username" &>/dev/null; then
+        echo "User does not exist"
+    else
+        read -p "enter new pssword: " password
+        read -p "reype new password: " password_retyped
+
+        if [ "$password" = "$password_retyped" ]; then #spacing syntax is crucial
+            echo "$username:$password" | sudo chpasswd #chpasswd tkaes username and pass 
+            echo "password  successfull<< changed"
+
+        else
+            echo "passwords dont match"
+        fi
+
+    fi
+}
+
+
+function delete_user {
+
+    read -p "eneter user that you want to reset:  " username
 
     if ! id "$username" &>/dev/null; then
         echo "User does not exist"
@@ -30,6 +52,22 @@ function delete_user {
         echo "user deleted"
 
     fi
+}
+
+
+function list_users {
+    cat /etc/passwd | awk -F: '{ print "Username: " $1 " (UID: " $3 ")" }'
+}
+
+
+function display_usage { 
+    echo "Usage: $0 [OPTIONS]" #$0 ) function name
+    echo "Options:"
+    echo "  -c, --create     Create a new user account."
+    echo "  -d, --delete     Delete an existing user account."
+    echo "  -r, --reset      Reset password for an existing user account."
+    echo "  -l, --list       List all user accounts on the system."
+    echo "  -h, --help       Display this help and exit."
 }
 
 if  [ $# -gt 0 ]; then #checks if number of arguments is greater than 0
@@ -42,6 +80,15 @@ if  [ $# -gt 0 ]; then #checks if number of arguments is greater than 0
 
         delete_user
 
+    elif [ "$1" = "-r" ] || [ "$1" = "--reset" ]; then
+        reset_password
+
+    elif [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
+        list_users
+
+     elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        display_usage
+
     else
         echo "invalid argument"
 
@@ -49,5 +96,5 @@ if  [ $# -gt 0 ]; then #checks if number of arguments is greater than 0
     fi
 
 else
-    echo "please set an argument"
+    display_usage
 fi
